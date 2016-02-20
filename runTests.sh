9bd0a93ddc98c613893b304487bba7bbc4b678eb
Michael Kenney <michael.kenney@returnpath.net>
2016-02-20 04:45:52 -0700

    Added managing PHPUnit container names

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,9 @@
 #!/bin/env sh
-#set -euo pipefail
 
-docker run -v $(pwd):/app:rw mkenney/phpunit -c /app/test/phpunit.xml
+container_name=phpunit-temp
+if [[ $(docker ps -a | grep $container_name) ]]; then
+    docker rm -f $container_name > /dev/null
+fi
+docker run --name=$container_name -v $(pwd):/app:rw mkenney/phpunit -c /app/test/phpunit.xml
+docker rm -f $container_name > /dev/null
 
