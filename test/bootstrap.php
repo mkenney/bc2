5dc63d29a395e68b222a2b66ce23074488a8ca7f
Michael Kenney <michael.kenney@returnpath.net>
2016-02-21 15:59:41 -0700

    Unit test cleanup

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
-require_once('vendor/autoload.php');
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+ini_set('memory_limit', '1024M');
+set_time_limit(0);
+require_once('vendor/autoload.php');
